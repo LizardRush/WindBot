b31527328065c9d8aed50ada067055a8195d0b5f
LizardRush <151949502+LizardRush@users.noreply.github.com>
2023-12-11 00:47:39 +0000

    hlep

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ terminal_channel_name = 'terminal'
 async def on_ready():
     print('Logged In With Token MTE3MDM5MDkzNjY0NDIzNTMxNQ...')
 
-@client.event
-async def recover_server(guild):
+async def recover_server(guild, message):
     # Delete existing channels and categories
     for channel in guild.channels:
         await channel.delete()
@@ -38,11 +37,11 @@ async def recover_server(guild):
 
         if category_name == 'VC':
             await category.create_voice_channel('General')
+
     await send_rules_message(guild)
     await create_terminal(guild)
-# rules message
+
 async def send_rules_message(guild):
-    # Send rules message in the 'rules' channel
     rules_channel = discord.utils.get(guild.channels, name='rules', type=discord.ChannelType.text)
     if rules_channel:
         rules_message = """RULES:
@@ -55,7 +54,7 @@ async def send_rules_message(guild):
         await rules_channel.send(rules_message)
     else:
         print("The 'rules' channel does not exist.")
-# create terminal
+
 async def create_terminal(guild):
     terminal_channel = discord.utils.get(guild.channels, name='terminal', type=discord.ChannelType.text)
     if not terminal_channel:
@@ -64,12 +63,15 @@ async def create_terminal(guild):
             guild.me: discord.PermissionOverwrite(read_messages=True)
         }
         new_terminal_channel = await guild.create_text_channel('terminal', overwrites=overwrites)
-        await new_terminal_channel.send("Terminal channel created successfully.")
+        await terminal_channel.send("Terminal channel created successfully.")
     else:
         await terminal_channel.send("Terminal channel already exists.")
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
     if message.content.startswith('$send_rules_message') and message.author.guild_permissions.administrator:
         terminal_channel = discord.utils.get(message.guild.channels, name='terminal', type=discord.ChannelType.text)
         if not terminal_channel:
@@ -83,24 +85,23 @@ async def on_message(message):
             await send_rules_message(message.guild)
         else:
             await message.channel.send("You can only send the rules message in the terminal channel.")
-    # create terminal command
+
     if message.content.startswith('$create_terminal') and message.author.guild_permissions.administrator:
         await create_terminal(message.guild)
-    # sever recovery command
+
     if message.content.startswith('$recover_server') and message.author.guild_permissions.administrator:
         terminal_channel = discord.utils.get(message.guild.channels, name=terminal_channel_name, type=discord.ChannelType.text)
 
         if terminal_channel and message.channel == terminal_channel:
-            # Server recovery initiated from the terminal channel
             await message.channel.send("Initiating server recovery...")
-            await recover_server(message.guild)
+            await recover_server(message.guild, message)
         elif not terminal_channel:
-            # Terminal channel doesn't exist; proceed with server recovery
             await message.channel.send("Terminal channel doesn't exist. Initiating server recovery...")
-            await recover_server(message.guild)
+            await recover_server(message.guild, message)
         else:
             await message.channel.send("You can only use the server recovery command in the terminal channel.")
-# recover the server
+
+# Recover the server using your bot token
 try:
     token = os.getenv("TOKEN") or ""
     if token == "":
@@ -108,11 +109,7 @@ try:
     client.run(token)
 except discord.HTTPException as e:
     if e.status == 429:
-        print(
-            "The Discord servers denied the connection for making too many requests"
-        )
-        print(
-            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
-        )
+        print("The Discord servers denied the connection for making too many requests")
+        print("Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests")
     else:
         raise e
